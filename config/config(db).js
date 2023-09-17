c845f54722395c2d4691d5633ddff5a06210b4df
Drip - Senior Developer <91119162+DripMicro@users.noreply.github.com>
2023-09-17 22:48:35 +0900

    Update config(db).js

--- config/config(db).js
+++ config/config(db).js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/CandleStick_TradePal").then(() => {
+mongoose.connect("mongodb://98.227.46.119:27017/CandleStick_TradePal").then(() => {
     console.log("Database connected!");
 }).catch((err) => {
     console.log(err, " error");
